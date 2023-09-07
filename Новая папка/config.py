a8d8a31323ef74c00b3ffb77c997b8ee8396afd4
BOGDANMIROSHNICHENKO <99182841+BOGDANMIROSHNICHENKO@users.noreply.github.com>
2023-09-07 15:47:11 +0300

    Add files via upload

---
+++ Новая папка/config.py
@@ -0,0 +1,9 @@
+from aiogram import Bot, Dispatcher, executor, types, asyncio, filters
+
+TOKEN='6568046818:AAHmPLWNLQoZNQ9lcqMBnq_fKYLNnfSg_8A'
+
+WORDS=['test', 'ютубер','тест']
+
+admins = [1083597384]  
+
+RESPECT = ['спасибо', 'спс', 'сяб', 'благодарю']

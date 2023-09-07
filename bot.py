eff0506351cb3659ee46752e85ede151aee17ab6
BOGDANMIROSHNICHENKO <99182841+BOGDANMIROSHNICHENKO@users.noreply.github.com>
2023-09-07 15:56:39 +0300

    bot.py

--- bot.py
+++ bot.py
@@ -12,7 +12,7 @@ dp = Dispatcher(bot, storage=storage)
 bot = Bot(token=cfg.TOKEN)
 
 
-kb =  types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=1)
+kb =  types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=1) #button menu
 kb2 = types.KeyboardButton('🧑‍💻 My commands 🧑‍💻:')
 kb3 = types.KeyboardButton('🧑‍💻 Main menu 🧑‍💻')
 kb4 = types.KeyboardButton('🧑‍💻 Owner 🧑‍💻')
@@ -27,60 +27,60 @@ async def new_chat_member_handler(message: types.Message):
 @dp.message_handler(content_types=types.ContentType.LEFT_CHAT_MEMBER) #farewell to departed users
 async def left_chat_member_handler(message: types.Message):
     member = message.left_chat_member
-    await message.reply(f"До свидания, {member.full_name} (@{member.username})!")
+    await message.reply(f"Goodbye, {member.full_name} (@{member.username})!")
     
 
-@dp.message_handler(commands=['start'])
+@dp.message_handler(commands=['start']) #Button Handler
 async def startbot(message: types.Message):
-   await message.answer(f'Hello {message.from_user.first_name},выбери одну из кнопок ниже для продолжения', reply_markup=kb)    
+   await message.answer(f'Hello {message.from_user.first_name}, choose one of the buttons below to continue', reply_markup=kb)    
 
 @dp.message_handler(Text(equals="🧑‍💻 My commands 🧑‍💻:"))
 async def kommenu(message: types.Message):
-      await message.reply(f"{message.from_user.first_name}, специально для вас я покажу свои команды \n Мои команды: /start, \n /ban (для администрации)")
+      await message.reply(f"{message.from_user.first_name}, especially for you, I will show my commands \n My commands: /start, \n /ban (for administration)")
 
 @dp.message_handler(Text(equals="🧑‍💻 Main menu 🧑‍💻"))
 async def kommenu2(message: types.Message):   
-   await message.reply(f"{message.from_user.first_name}, <b>главное меню</b> по вашей просьбе", reply_markup=kb, parse_mode=types.ParseMode.HTML)
+   await message.reply(f"{message.from_user.first_name}, <b>main menu</b> at your request", reply_markup=kb, parse_mode=types.ParseMode.HTML)
 
 @dp.message_handler(Text(equals="🧑‍💻 Owner 🧑‍💻"))
 async def kommenu3(message: types.Message):   
-   await message.reply(f"{message.from_user.first_name}, <b>Данный бот был сделан разработчиком <a href='https://t.me/Bogdan_Mirosh'>WEBBOGDAN</a></b>, чтобы поддержать нашего с вами любимца) подпишитесь на его <a href='https://github.com/BOGDANMIROSHNICHENKO'><b>GITHUB</b></a>", reply_markup=kb, parse_mode=types.ParseMode.HTML)
+   await message.reply(f"{message.from_user.first_name}, <b><b>This bot was made by the developer <a href='https://t.me/Bogdan_Mirosh'>WEBBOGDAN</a></b> to support our pet) subscribe to his <a href='https ://github.com/BOGDANMIROSHNICHENKO'><b>GITHUB</b></a>", reply_markup=kb, parse_mode=types.ParseMode.HTML)
 
 
 logging.basicConfig(level=logging.INFO)
 
 
-@dp.message_handler(Command("ban"))
+@dp.message_handler(Command("ban")) #command ban
 async def ban_user(message: types.Message, state: FSMContext):
     if message.from_user.id in cfg.admins:  
         if message.reply_to_message:  
             user_id = message.reply_to_message.from_user.id  
             try:
                 await bot.kick_chat_member(message.chat.id, user_id)  
-                await message.reply(f"Пользователь {user_id} был забанен.")  
+                await message.reply(f"User {user_id} was banned.")  
             except Exception as e:
-                await message.reply("Произошла ошибка при бане пользователя.")
-                logging.error(f"Ошибка бана пользователя: {e}")
+                await message.reply("An error occurred while banning a user.")
+                logging.error(f"User ban error: {e}")
         else:
-            await message.reply("Ответьте на сообщение пользователя, которого хотите забанить.")
+            await message.reply("Reply to the message of the user you want to ban.")
     else:
-        await message.reply("Вы не являетесь администратором группы.")
+        await message.reply("You are not a group administrator.")
 
 @dp.message_handler()
-async def mess_handler(message: types.Message):     
+async def mess_handler(message: types.Message):    #filter bad words 
     text = message.text.lower()
     text2 = message.text.lower().replace(' ', '')
     for word in cfg.WORDS:
         if word in text:
             await message.delete()
-            await message.answer( 'Ругаться плохо') 
+            await message.answer( 'Swear badly') 
         if word in text2:
             await message.delete()
-            await message.answer( 'Ругаться плохо')
+            await message.answer( 'Swear badly')
     if message.reply_to_message:
         for respmes in cfg.RESPECT:
             if respmes in text:
-                await message.reply(f'Уважание оказано пользователем @{message.from_user.username}')
+                await message.reply(f'Уважание оказано пользователем @{message.from_user.username}') #respect to user
 
 executor.start_polling(dp, skip_updates=True)
 
